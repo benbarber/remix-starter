05842daf62a0629885f2206f7097d1d4838bf96b
Ben Barber <contact@benbarber.co.uk>
2023-09-25 13:38:18 +0100

    Update linters

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,12 +2,10 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 
-      "New Remix App" },
-    { name: "description", 
-    content: "Welcome to Remix!" },
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
   ];
-}
+};
 
 export default function Index() {
   return (
